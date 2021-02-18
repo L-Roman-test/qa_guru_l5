39466f49fa8d7c2127debeb3579c2deecf01cd54
roman.lelikov <roman.lelikov@brandmaker.com>
2021-02-18 19:49:16 +0700

    removed open() from BaseTest add /gradle/ /.idea/  to gitignore

--- src/test/java/settings/BaseTest.java
+++ src/test/java/settings/BaseTest.java
@@ -5,13 +5,10 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
-import static com.codeborne.selenide.Selenide.open;
-
 public class BaseTest {
     @BeforeAll
     public static void config() {
         Configuration.browserSize = "1920x1080";
         SelenideLogger.addListener("allure", new AllureSelenide());
-        open("https://github.com");
     }
 }
